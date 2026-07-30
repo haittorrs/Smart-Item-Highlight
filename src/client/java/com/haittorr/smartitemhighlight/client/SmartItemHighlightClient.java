6795bd3548751a3b73f8ffbd9ecdebe307e02d15
Haider <161240189+haittorrv2@users.noreply.github.com>
2026-07-30 22:27:52 +0100

    Set up initial Fabric mod project

---
+++ src/client/java/com/haittorr/smartitemhighlight/client/SmartItemHighlightClient.java
@@ -0,0 +1,10 @@
+package com.haittorr.smartitemhighlight.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmartItemHighlightClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
